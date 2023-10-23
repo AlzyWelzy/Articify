1decb55a8517431ca68ed5a759aedad4e63124bd
AlzyWelzy <welzyalzy@gmail.com>
2023-10-23 22:32:59 +0530

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import Hero from "./components/Hero";
 import Demo from "./components/Demo";
 import "./App.css";
 
-const App = () => (
-  <main>
-    <div className="main">
-      <div className="gradient" />
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
 
       <div className="app">
         <Hero />
         <Demo />
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
 
 export default App;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { assets } from "../assets";
-const articify = assets.articify;
+const logo = assets.articify;
 
 // Your code that uses these imported assets
 
@@ -7,20 +7,25 @@ const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img
-          src={articify}
-          alt="articify_logo"
-          className="w-28 object-contain"
-        />
+        <img src={logo} alt="sumz_logo" className="w-9 object-contain" />
 
         <button
           type="button"
           onClick={() => window.open("https://github.com/AlzyWelzy/articify")}
           className="black_btn"
         >
-          Github
+          GitHub
         </button>
       </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient ">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Articify, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
     </header>
   );
 };
