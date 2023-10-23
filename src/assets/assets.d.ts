15390c04e04e663532be361c468d97f600be58d8
AlzyWelzy <welzyalzy@gmail.com>
2023-10-23 21:36:23 +0530

    adding logo

---
+++ src/assets/assets.d.ts
@@ -0,0 +1,13 @@
+// assets.d.ts
+declare module "assets" {
+  const assets: {
+    linkIcon: string;
+    loader: string;
+    copy: string;
+    logo: string;
+    tick: string;
+    articify: string;
+  };
+
+  export = assets;
+}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,27 @@
-const Hero = () => <></>;
+import { assets } from "../assets";
+const articify = assets.articify;
 
+// Your code that uses these imported assets
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center flex-col">
+        <img
+          src={articify}
+          alt="articify_logo"
+          className="w-28 object-contain"
+        />
+
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/AlzyWelzy/articify")}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+    </header>
+  );
+};
 export default Hero;
